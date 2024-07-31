ca5087e38c362a4e6a7d6bef0193c8786af2809d
Shanks0304 <dev.top.guru@gmail.com>
2024-07-31 12:18:55 +0300

    standardize the model

--- utils/source.py
+++ utils/source.py
@@ -1,16 +1,14 @@
+import time
 from pyannote.audio import Pipeline
 from transformers import pipeline
-from speechbox import ASRDiarizationPipeline
+from utils.diarize import ASRDiarizationPipeline
 import os
 import torchaudio
 
-
-AUTH_TOKEN = os.getenv("AUTH_TOKEN")
-
-
 def get_audio_files(directory):
-    project_dir =  os.path.dirname(os.path.abspath(__file__))
+    project_dir =  os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     download_folder = os.path.join(project_dir, directory)
+    print("Directory path: ", download_folder)
     # Expanded list of recognized audio file extensions  
     audio_extensions = [  
         '.3gp',  '.aa',   '.aac',  '.aax',  '.act',  '.aiff', '.alac',  
@@ -29,13 +27,11 @@ def get_audio_files(directory):
         for file in files:  
             if any(file.lower().endswith(ext) for ext in audio_extensions):  
                 audio_files.append(os.path.join(root, file))  
-
     return audio_files 
 
-def diarization():
+def asr_diarization():
     diarization_pipeline = Pipeline.from_pretrained(
         "pyannote/speaker-diarization-3.1",
-        use_auth_token=AUTH_TOKEN
     )
     asr_pipeline = pipeline(
         "automatic-speech-recognition",
@@ -45,25 +41,26 @@ def diarization():
     speech_pipeline = ASRDiarizationPipeline(
         asr_pipeline = asr_pipeline, diarization_pipeline = diarization_pipeline
     )
-    diarization_result = []
     audio_file_list = get_audio_files('download')
-    for audio_file in audio_file_list:
-        diarization_result.append(speech_pipeline(audio_file))
-    return diarization_result
-
-
-def speaker_diarization():
-    diarization_pipeline = Pipeline.from_pretrained(
-        "pyannote/speaker-diarization-3.1",
-        use_auth_token=AUTH_TOKEN
-    )
-    audio_file_list = get_audio_files('download')
-    diarizations = []
     print(audio_file_list)
     for audio_file in audio_file_list:
-        waveform, sample_rate = torchaudio.load(audio_file)
-        diarizations.append(diarization_pipeline({"waveform": waveform, "sample_rate": sample_rate}, num_speakers=2))
-    return diarizations
+        file_name = audio_file.split('\\')[-1]
+        start_time = time.time()
+        result = format_as_transcription(speech_pipeline(audio_file))
+        with open(f'download\\{file_name}.txt', 'w') as file:
+            file.write(result)
+        print(f"Elapsed Time for {file_name}: ", time.time() - start_time)
+
+def tuple_to_string(start_end_tuple, ndigits=1):
+    return str((round(start_end_tuple[0], ndigits), round(start_end_tuple[1], ndigits)))
+
+def format_as_transcription(raw_segments):
+    return "\n\n".join(
+        [
+            chunk["speaker"] + " " + tuple_to_string(chunk["timestamp"]) + chunk["text"]
+            for chunk in raw_segments
+        ]
+    )
 
 def speech_to_text():
     asr_pipeline = pipeline(
@@ -78,7 +75,6 @@ def speech_to_text():
         return_timestamps=True,
     )["chunks"]
     print(text_result)
-
 
 def audio_convert_to_wav(audio_file):  
     if audio_file.lower().endswith('.wav'):  
