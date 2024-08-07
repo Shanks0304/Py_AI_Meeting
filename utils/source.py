74f04dc75f55f994e131a8c5a45b9efdbea2d2d1
Shanks0304 <dev.top.guru@gmail.com>
2024-08-07 19:17:04 +0300

    update whisper model and transcription accuracy high

--- utils/source.py
+++ utils/source.py
@@ -1,8 +1,7 @@
 import time
 from pyannote.audio import Pipeline
 import torchaudio
-from transformers import pipeline
-from utils.faster_whisper_model import faster_whisper_test
+from transformers import pipeline, AutoModelForSpeechSeq2Seq, AutoProcessor
 from utils.diarize import ASRDiarizationPipeline
 import os
 import librosa
@@ -35,27 +34,24 @@ def get_audio_files(directory):
                 audio_files.append(os.path.join(root, file))  
     return audio_files 
 
-def asr_diarization():
+def asr_diarization(filepath: str):
     diarization_pipeline = Pipeline.from_pretrained(
         "pyannote/speaker-diarization-3.1",
     )
     asr_pipeline = pipeline(
         "automatic-speech-recognition",
-        model="openai/whisper-medium.en",
+        model="openai/whisper-large-v3",
         chunk_length_s=30,
     )
     speech_pipeline = ASRDiarizationPipeline(
         asr_pipeline = asr_pipeline, diarization_pipeline = diarization_pipeline
     )
-    audio_file_list = get_audio_files('download')
-    print(audio_file_list)
-    for audio_file in audio_file_list:
-        file_name = audio_file.split('\\')[-1]
-        start_time = time.time()
-        result = format_as_transcription(speech_pipeline(audio_file))
-        with open(f'download\\{file_name}.txt', 'w') as file:
-            file.write(result)
-        print(f"Elapsed Time for {file_name}: ", time.time() - start_time)
+    file_name = filepath.split('/')[-1]
+    start_time = time.time()
+    result = format_as_transcription(speech_pipeline(filepath))
+    with open(f'download\\{file_name}.txt', 'w') as file:
+        file.write(result)
+    print(f"Elapsed Time for {file_name}: ", time.time() - start_time)
 
 def tuple_to_string(start_end_tuple, ndigits=1):
     return str((round(start_end_tuple[0], ndigits), round(start_end_tuple[1], ndigits)))
@@ -71,14 +67,11 @@ def format_as_transcription(raw_segments):
 def speech_to_text(filepath: str):
     asr_pipeline = pipeline(
         "automatic-speech-recognition",
-        model="openai/whisper-medium.en",
+        model="openai/whisper-large-v3",
         chunk_length_s=30,
     )
-    audio_file_list = get_audio_files('download')
-    print(audio_file_list[0])
-    print(type(audio_file_list[0]))
     text_result = asr_pipeline(
-        audio_file_list[0],
+        filepath,
         batch_size=8,
         return_timestamps=True,
     )["chunks"]
@@ -100,7 +93,7 @@ def transcribe_audio(filename):
     temp_file_path = os.path.join(project_dir, cleaned_file_path)
     print(temp_file_path)
     # audio_file= open(temp_file_path, "rb")
-    speaker_diarization(temp_file_path)
+    asr_diarization(temp_file_path)
 
 def speaker_diarization(filepath: str):
     diarization_pipeline = Pipeline.from_pretrained(
